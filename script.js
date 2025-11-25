adead5da64155fc6668f7198a6f512023c55b1b9
ABO0069 <ayush.biswas@g.bracu.ac.bd>
2025-11-25 23:46:23 +0600

    Animation added

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 const menuBtn = document.getElementById('menu-btn');
 const mobileMenu = document.getElementById('mobile-menu');
-// Select all links inside the mobile menu
 const mobileLinks = mobileMenu.querySelectorAll('a');
-
-// 1. Toggle Menu when Hamburger is clicked
 menuBtn.addEventListener('click', () => {
     mobileMenu.classList.toggle('hidden');
 });
-
-// 2. Close Menu when a Link is clicked
 mobileLinks.forEach(link => {
     link.addEventListener('click', () => {
         mobileMenu.classList.add('hidden');
